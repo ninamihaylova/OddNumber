70a41658d59c0e2d81a616dff37fa2182360d159
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 23:35:10 +0300

    Initial commit

---
+++ OddNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿class OddNumber
+{
+    public static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        while (num %2==0)
+        {
+            num = int.Parse(Console.ReadLine());
+            
+        }
+        Console.WriteLine(num);
+    }
+}
